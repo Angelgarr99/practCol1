2bb2a23fc142b00d057a7842fc3a71e118c1da6f
Angel Armando Garrido Aguirre <angelarmando.garrido@utxicotepec.edu.mx>
2020-06-12 15:53:24 -0500

    prodecimiento del primer video

---
+++ src/app/componentes/saludar.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component(
+    {
+        selector: 'saludar-app',
+        templateUrl: './saludar.component.html',
+        styleUrls:['./saludar.component.css'] 
+    }
+)
+export class saludarComponent{
+
+}
