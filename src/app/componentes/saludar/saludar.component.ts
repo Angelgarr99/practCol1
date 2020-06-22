954ad420b16d36039ec5d5c1a950fc9afd1508cd
Libni-Mendoza <libni.heldai@gmail.com>
2020-06-22 15:53:28 -0500

    Directiva ngClass

--- src/app/componentes/saludar/saludar.component.ts
+++ src/app/componentes/saludar/saludar.component.ts
@@ -27,4 +27,12 @@ export class saludarComponent{
         this.nombreLista='';
         console.log(this.nombres);
     }
+
+    validando(texto:string){
+        return{
+            'weak': texto.length < 4, 
+            'medium': texto.length >=4 && texto.length<=7,
+            'strong': texto.length > 7
+        }
+    }
 }
